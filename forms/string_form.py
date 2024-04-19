5c486656bd3dc2d77d7b425ff6f1b7a652376486
engelhartrueben <engelhartrueben@gmail.com>
2024-04-19 18:11:48 -0400

    add formatting ideas

--- forms/string_form.py
+++ forms/string_form.py
@@ -1,5 +1,15 @@
 """String form to check user input is flaot"""
 
+"""
+FORMATTING IDEAS ---
+Math        : Math
+enGlIsh     : English
+maTH 207    : Math 207
+137 CS      : 137 CS
+% soci 3033 : Soci 3033
+poli sci 1  : Poli Sci 1 
+"""
+
 class StringForm():
     def check_string(self, input):
         # Check if its a string type
