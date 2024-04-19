143ccd96594adc249fd0079d1746b8e231ff4c24
engelhartrueben <engelhartrueben@gmail.com>
2024-04-19 16:48:02 -0400

    float form basic

---
+++ forms/float_form.py
@@ -0,0 +1,15 @@
+
+
+class FloatForm():
+    def check_integer(self, input):
+        # Check if its a string type
+        if not isinstance(input, str):
+            return None
+        else:
+            input = input.strip()
+            try:
+                float = float(input)
+                return float
+            except ValueError:
+                return None
+            
