923a6bfc1cd15099d488626df4199339149d247c
engelhartrueben <engelhartrueben@gmail.com>
2024-04-19 16:01:05 -0400

    init string form test

---
+++ test/forms/test_string_form.py
@@ -0,0 +1,33 @@
+from forms.string_form import strForm
+
+def test_string_form():
+    test_cases = {
+        'pass_test_cases' : {
+            'str_pass_1' : 'Math',
+            'str_pass_2' : 'enGlIsh',
+            'str_pass_3' : 'maTH 207',
+            'str_pass_4' : '137 CS',
+            # bit of user freedom with this one,
+            # can't control everything
+            'str_pass_5' : '% soci 3033',
+            'str_pass_6' : '6578'
+        },
+        'fail_test_cases' : {
+            'int_fail_1' : 25,
+            'int_fail_2' : -3,
+            'int_fail_3' : 0,
+            'flt_fail_1' : 2.2,
+            'flt_fail_2' : -905.6,
+            # unsure about testing types
+            'type_obj_fail' : {'really' : 'silly'},
+            'type_tuple_fail' : (2, 'really',),
+            'type_set_fail' : {'peach', 'mango'},
+            'type_list_fail' : ['wolf', 3]
+        }
+    }
+
+    for test_suits, _ in test_cases:
+        for _, val in test_cases[test_suits]:
+            # Need to to decide what is returned
+            # to show proper/improper input
+            assert strForm(val)
