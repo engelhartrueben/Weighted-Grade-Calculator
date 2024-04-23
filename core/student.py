e016bdac26b4dc235aa6ff94b321847fab1339af
engelhartrueben <engelhartrueben@gmail.com>
2024-04-23 18:14:45 -0400

    add the beginning logic for grabbing grades per student

--- core/student.py
+++ core/student.py
@@ -3,14 +3,26 @@ Create class for each student that conatins the logic for
 their grades, days missing, and names
 """
 from forms.name_form import NameForm
+from forms.integer_form import IntegerForm
+from core.engine import AppEngine
 
-class Student:
+class Student(AppEngine):
     def __init__(self):
         self.student_name = None
+        self.assignment_grades = None
+        self.assignment_overall_grade = None
+        self.quiz_grades = None
+        self.quiz_overall_grade = None
+        self.project_grades = None
+        self.project_overall_grade = None
         self.absent_count = None
+        self.absent_grade = None
 
     def run(self):
-        self.student_name = self.get_student_name()
+        self.student_name = self.get_student_name(self)
+        self.get_assignment_grades = self.get_assignment_grades(self, self.assignment_count)
+        self.get_quiz_grades = self.get_quiz_grades(self, self.quiz_count)
+        self.get_project_grades = self.get_project_grades(self, self.project_count)
 
     def get_student_name(self):
         """sets students name"""
@@ -19,11 +31,24 @@ class Student:
         # studen. i.e. an entire class
         prompt = "Enter Student's Name: "
         while not self.student_name:
-            cmd = input(prompt)
-            name = NameForm.check_name(cmd)
-            if not name:
+            cmd = NameForm.check_name(self, input(prompt))
+            if not cmd:
                 # not sending to self.message
                 # containing to this func
                 print("Invalid Student Name!")
             else:
-                self.name = name
+                self.name = cmd
+                break
+    
+    def get_assignment_grades(self, count):
+        pass
+
+    def get_quiz_grades(self, count):
+        pass
+
+    def get_project_grades(self, count):
+        pass
+
+    def get_absent_amount(self):
+        pass
+
