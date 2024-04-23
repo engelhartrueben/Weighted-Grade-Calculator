3d4431905087b2a09e1883a8050df0f7590354ea
engelhartrueben <engelhartrueben@gmail.com>
2024-04-23 09:33:59 -0400

    inital of app and engine

--- app.py
+++ app.py
@@ -1,5 +1,42 @@
+from core.engine import AppEngine
+
+class App:
+    def __init__(self, grade_items=None):
+        self.engine = AppEngine(grade_items)
+
+    def run(self):
+        """rotatin function for user input"""
+        # Add in intial welcomd message here
+        print("Welcome to Weighted Grade Calculator!"/
+              " At any time, type '-h' for help!")
+        # Would like to add a basic 2d render
+        while True:
+            prompt = "one thing or another"
+            cmd = input(prompt)
+            self.execute_command(cmd)
+            
+            # Exit logic
+            if not self.engine.continue_execution:
+                break
+
+    def execute_command(self, cmd):
+        if cmd in ('q', 'quit', 'exit'):
+            self.engine.continue_execution = False
+            # Maybe add logic to save current work.
+            # tinyDB?
+            self.engine.message = "Have a nice day!"
+        elif cmd in ('-h', '-help'):
+            # call on a preformatted help
+            # message
+            self.engine.message = "this or that"
+
+if __name__ == '__main__':
+    grade_items = [
+        'Quizzes',
+        'Projects',
+        'Assignments',
+        'Participation'
+    ]
+    engine = AppEngine(grade_item=grade_items)
 
 
-class App():
-    def engine(self):
-        pass
--- core/engine.py
+++ core/engine.py
@@ -1,5 +1,13 @@
 
 
 class AppEngine():
-    def execution(self, cmd):
+    def __init__(self, grade_item=None):
+        self.message = None
+        self.continue_execution = True
+        self.grade_item = grade_item
+
+    def add_grade(self):
+        pass
+
+    def calculate_grade(self):
         pass
