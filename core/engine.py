7799891852c8706fc4de63b64c11777f1880a823
engelhartrueben <engelhartrueben@gmail.com>
2024-04-23 10:46:41 -0400

    add get_student_name

--- core/engine.py
+++ core/engine.py
@@ -1,11 +1,22 @@
+from forms.name_form import NameForm
 
-
-class AppEngine():
+class AppEngine:
     def __init__(self, grade_item=None):
         self.message = None
         self.continue_execution = True
         self.grade_item = grade_item
+        self.student_name = None
 
+    def get_student_name(self):
+        prompt = "Enter Student's Name: "
+        while not self.student_name:
+            cmd = input(prompt)
+            name = NameForm.check_name(cmd)
+            if not name:
+                self.message("Invalid Name!")
+            else:
+                self.name = name
+            
     def add_grade(self):
         pass
 
@@ -19,4 +30,4 @@ class AppEngine():
     def help(self):
         """Help comments"""
         # Add this in towards the end?
-        self.message = None
+        self.message = "PLACE HOLDER"
